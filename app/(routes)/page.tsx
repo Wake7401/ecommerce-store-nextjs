299421533a7f9df5037835dd90703cb047894840
Wake7401 <91364482+Wake7401@users.noreply.github.com>
2024-04-08 13:45:13 +0700

    create billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,19 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("55956b2e-9792-40e7-8322-ae864018aee9");
     return ( 
-        <div>Hello</div>
+      <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
+      </div>
+    </Container>
      );
 }
  
